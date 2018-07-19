f5ac2ce8de7d57b95503b9cb9f32afa3b20287ef
alen <alen.c@anvo.com>
2018-07-19 14:42:03 +0800

    新增依赖jar

--- src/cn/Config.java
+++ src/cn/Config.java
@@ -2,11 +2,11 @@ package cn;
 
 public class Config {
 	
-	 final static String mchId="10000000";
+	 final static String mchId="10000001";
 	 final static String reqKey="fund@letiantaotao.com";
 	 final static String resKey="fund@letiantaotao.com";
-	 final static String notifyUrl="http://10.12.179.77:8888/NotifyOrderServlet";
-	 final static String payUrl="http://10.12.179.110:3020/pay/create_order";
-	 final static String qryUrl="http://10.12.179.110:3020/pay/query_order";
+	 final static String notifyUrl="http://www.baidu.com";
+	 final static String payUrl="http://122.114.185.56:3020/pay/create_order";
+	 final static String qryUrl="http://122.114.185.56:3020/pay/query_order";
 
 }
